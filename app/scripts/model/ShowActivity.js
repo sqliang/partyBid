1d9e1d270923f0be1712d1397c55e44ee2ee3a0e
minchina <minchina91@gmail.com>
2014-07-28 17:20:52 +0800

    deleted:old wrong model

--- app/scripts/model/ShowActivity.js
+++
@@ -1,6 +0,0 @@
-function ShowActivity (){
-
-    var arr1=JSON.parse(localStorage['activitykey']);
-    arr1 = arr1.reverse();
-    return arr1;
-}
--- app/scripts/model/ShowUsers.js
+++
@@ -1,5 +0,0 @@
-function ShowUsersList (argument) {
-    var result = JSON.parse(localStorage['during_activity']).name;
-    var users = JSON.parse(localStorage[result]);
-    return users;	
-}
